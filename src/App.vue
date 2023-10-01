4524d0f823f00723d27f8fde09ed0fc45588e5ac
camilo nunez <camilonunezz9812@gmail.com>
2023-09-30 19:15:59 -0500

    feat: app.vue, main.ts and public assets init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <router-view v-slot="{ Component }">
+    <transition name="fade" mode="out-in">
+      <component :is="Component" />
+    </transition>
+  </router-view>
+</template>
+
+<style scoped lang="css">
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s ease-in-out;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { createApp } from 'vue'
+import { createI18n } from "vue-i18n";
+import App from './App.vue'
+import router from './routes';
+import en from "./locale/en.json"
+import es from "./locale/es.json"
+import './style/index.css'
+
+navigator.serviceWorker.register('/sw.js')
+
+const i18n = createI18n({
+  legacy: false,
+  globalInjection: true,
+  locale: "es",
+  fallbackLocale: "es",
+  messages: { en, es },
+});
+
+const app = createApp(App)
+app.use(i18n)
+app.use(router)
+app.mount('#app')
