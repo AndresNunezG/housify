e259b64423d4f8f2061c5c8fba02bade823e2731
camilo nunez <camilonunezz9812@gmail.com>
2023-09-30 19:09:15 -0500

    feat: vite, typescript and editorconfig configs

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from "url";
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
+      { find: '@assets', replacement: fileURLToPath(new URL('./src/assets', import.meta.url)) },
+      { find: '@shared', replacement: fileURLToPath(new URL('./src/shared', import.meta.url)) },
+    ]
+  }
+})
